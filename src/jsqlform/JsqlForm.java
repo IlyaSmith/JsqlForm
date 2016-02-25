e14277711c399850d54c1e0d459faa036b4cd299
IlyaSmith <kuznetsovilushka@gmail.com>
2016-02-25 09:31:39 +0300

    Вынесен в отдельный метод connection л БД

--- src/jsqlform/JsqlForm.java
+++ src/jsqlform/JsqlForm.java
@@ -5,6 +5,8 @@
  */
 package jsqlform;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -20,42 +22,57 @@ import javax.swing.*;
  *
  * @author Ilya
  */
-public class JsqlForm {
+public class JsqlForm extends JFrame {
 
     /**
      * @param args the command line arguments
      */
     // параметры подключения
-private static String
+private String
 //dsn = "jdbc:mysql://localhost:3306/affablebean",
 dsn =  "jdbc:mysql://10.0.68.15:3306/asterisk",
 queryString = "SELECT * FROM cdr where calldate > '2016-01-24 00:00:00.0'",
 uid = "root",
 //pwd = "1234";
-pwd = "astRO#27";     
-public void setQuerySting (String arg) {
-    queryString = arg;
+pwd = "astRO#27";  
+
+private DatabaseTableModel dbm;
+
+public JsqlForm() {
+   
+    // наша модель
+dbm =
+new DatabaseTableModel(false);
+
+// таблица и окно
+JTable table = new JTable(dbm);
+setTitle ("Asterisk Loader");
+setSize(800, 700); 
+
+ // Handle window closing events.
+    addWindowListener(new WindowAdapter() {
+      public void windowClosing(WindowEvent e) {
+        actionExit();
+      }
+    });
+getContentPane().add(new JScrollPane(table));
+//
+setConection (queryString);
+ 
 }
-public static void main(String[] args) {
-// инициализация JDBC
-Connection conn = null;
+//JDBC
+private void setConection (String queryString) {
+ // инициализация JDBC
+   Connection conn = null;
 try {
 //Class.forName("com.mysql.jdbc.Driver");
 // объект-соединение с БД
 conn = DriverManager.getConnection(dsn, uid, pwd);
 Statement st = conn.createStatement();
 // выполняем запрос
-ResultSet rs = st.executeQuery(queryString );
-// наша модель
-DatabaseTableModel dbm =
-new DatabaseTableModel(false);
+ResultSet rs = st.executeQuery(queryString);
 
-// таблица и окно
-JTable table = new JTable(dbm);
-JFrame frame = new JFrame("Hi");
-frame.setSize(700, 700);
-frame.getContentPane().add(new JScrollPane(table));
-frame.show();
+
 
 // выводим результат запроса на экран
 dbm.setDataSource(rs);
@@ -64,7 +81,21 @@ conn.close();
 } catch (Exception ex) {
 throw new RuntimeException(ex);
 }
+    
+}
 
+public void setQuerySting (String arg) {
+    queryString = arg;
+}
+
+ // Exit this program.
+  private void actionExit() {
+    System.exit(0);
+  }
+  
+public static void main(String[] args) {
+JsqlForm jsf = new JsqlForm();
+jsf.show();
 }//end main 
    
     
